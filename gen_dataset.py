5474b843dc5545907fd8821ad9c91974ab60bcd2
rex <hanhung>
2020-06-14 23:25:40 +0800

    commit

--- gen_dataset.py
+++ gen_dataset.py
@@ -13,7 +13,7 @@ def gen_image(character, angle, shift, scale):
     M = cv2.getRotationMatrix2D(img_center, angle, 1)
     rotated_image = cv2.warpAffine(character, M, (cols, rows), borderValue=(255,255,255))
 
-    dim = (int(rows * scale), int(rows * scale))
+    dim = (int(rows * scale), int(cols * scale))
     rotated_image = cv2.resize(rotated_image, dim, interpolation = cv2.INTER_AREA)
     rows = rotated_image.shape[0]
     cols = rotated_image.shape[1]
@@ -31,10 +31,10 @@ def gen_image(character, angle, shift, scale):
     start = (dim[0] - rows) // 2
     end = start + rows
 
-    x_min = start+shift[0] + 6
-    x_max = end+shift[0] + 6
-    y_min = start+shift[1] + 6
-    y_max = end+shift[1] + 6
+    x_min = start+shift[0]+6
+    x_max = end+shift[0]+6
+    y_min = start+shift[1]+6
+    y_max = end+shift[1]+6
 
     img[start+shift[0]:end+shift[0], start+shift[1]:end+shift[1], :] = rotated_image
 
@@ -47,72 +47,65 @@ root = './handwriting'
 source = glob.glob(osp.join(root, '*.jpg'))
 
 normal_rotation = [0, -10, 10, -5, 5]
-normal_rotation = [-20]
 wrong_rotation = [20, -20, -30, 30]
 
-normal_scale = [1, 0.9]
-# wrong_scale = [0.8, 0.7, 0.6, 0.5]
-wrong_scale = [0.7]
+normal_scale = [0.88]
+wrong_scale = [0.8, 0.7, 0.6, 0.5]
 
 train_images = []
 train_bbox = []
 train_label = []
-# for i in tqdm(range(2000)):
-#     path = random.choice(source)
-#     img = cv2.imread(path, 1)
-#     dim = (90, 90)
-#     img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
-#     img, bbox = gen_image(img, random.choice(normal_rotation), (0, 0), random.choice(normal_scale))
-
-#     # cv2.rectangle(img, (bbox[0], bbox[2]), (bbox[1], bbox[3]), (0, 0, 0), 3)
-#     # cv2.imshow('image', img)
-#     # cv2.waitKey(0)
-#     # cv2.destroyAllWindows()
-#     # exit()
-
-#     train_images.append(img)
-#     train_bbox.append(bbox)
-#     train_label.append(0)
-
-for i in tqdm(range(2000)):
+train_samples = 50000
+test_samples = 10000
+for i in tqdm(range(train_samples)):
     path = random.choice(source)
     img = cv2.imread(path, 1)
     dim = (90, 90)
     img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
-    img, bbox = gen_image(img, random.choice(normal_rotation), (0, 0), random.choice(wrong_scale))
 
-    cv2.imwrite('./example/ex2.jpg', img)
-    exit()
-    cv2.rectangle(img, (bbox[0], bbox[2]), (bbox[1], bbox[3]), (0, 0, 0), 2)
-    cv2.imwrite('./example/return.jpg', img)
-    print((bbox[0], bbox[1], bbox[2], bbox[3]))
-    exit()
-    
+    rotation_choice = np.random.choice(2, 1)[0]
+    if rotation_choice == 0:
+        rotation_angle = random.choice((-1, 1)) * np.random.randint(low=0, high=11, size=1)[0]
+    else:
+        rotation_angle = random.choice((-1, 1)) * np.random.randint(low=20, high=41, size=1)[0]
+    # print('Rotation')
+    # print(rotation_choice)
+    # print(rotation_angle)
+
+    scale_choice = np.random.choice(2, 1)[0]
+    if scale_choice == 0:
+        scale_percentage = np.random.uniform(0.9, 1.0, 1)[0]
+    else:
+        scale_percentage = np.random.uniform(0.5, 0.7, 1)[0]
+    # print('Scale')
+    # print(scale_choice)
+    # print(scale_percentage)
+
+    shift_choice = np.random.choice(2, 1)[0]
+    shift_x = 0
+    shift_y = 0
+    if shift_choice == 0:
+        shift_x = random.choice((-1, 1)) * np.random.randint(low=0, high=4, size=1)[0]
+        shift_y = random.choice((-1, 1)) * np.random.randint(low=0, high=4, size=1)[0]
+    else:
+        shift_x = random.choice((-1, 1)) * np.random.randint(low=6, high=11, size=1)[0]
+        shift_y = random.choice((-1, 1)) * np.random.randint(low=6, high=11, size=1)[0]
+    # print('Shift')
+    # print(shift_choice)
+    # print(shift_x)
+    # print(shift_y)
+
+    img, bbox = gen_image(img, rotation_angle, (shift_x, shift_y), scale_percentage)
+    # cv2.rectangle(img, (bbox[2], bbox[0]), (bbox[3], bbox[1]), (255, 0, 0), 1)
+    # cv2.imshow('image', img)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+    # print([rotation_choice, scale_choice, shift_choice])
+    # exit()
+
     train_images.append(img)
     train_bbox.append(bbox)
-    train_label.append(1)
-
-for i in tqdm(range(2000)):
-    path = random.choice(source)
-    img = cv2.imread(path, 1)
-    dim = (90, 90)
-    img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
-    img, bbox = gen_image(img, random.choice(wrong_rotation), (0, 0), random.choice(normal_scale))
-    
-    train_images.append(img)
-    train_bbox.append(bbox)
-    train_label.append(2)
-
-for i in tqdm(range(2000)):
-    path = random.choice(source)
-    img = cv2.imread(path, 1)
-    dim = (90, 90)
-    img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
-    img, bbox = gen_image(img, random.choice(wrong_rotation), (0, 0), random.choice(wrong_scale))
-    
-    train_images.append(img)
-    train_bbox.append(bbox)
-    train_label.append(3)
+    train_label.append([rotation_choice, scale_choice, shift_choice])
 
 train_images = np.array(train_images)
 train_bbox = np.array(train_bbox)
@@ -120,65 +113,70 @@ train_label = np.array(train_label)
 np.save('./dataset/train_images.npy', train_images)
 np.save('./dataset/train_bbox.npy', train_bbox)
 np.save('./dataset/train_label.npy', train_label)
+print('Train Shapes')
+print(train_images.shape)
+print(train_bbox.shape)
+print(train_label.shape)
 
 ################################################################################################################################################
 
 train_images = []
 train_bbox = []
 train_label = []
-for i in tqdm(range(500)):
+for i in tqdm(range(test_samples)):
     path = random.choice(source)
     img = cv2.imread(path, 1)
     dim = (90, 90)
     img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
-    img, bbox = gen_image(img, random.choice(normal_rotation), (0, 0), random.choice(normal_scale))
 
-    # cv2.rectangle(img, (bbox[0], bbox[2]), (bbox[1], bbox[3]), (0, 0, 0), 3)
+    rotation_choice = np.random.choice(2, 1)[0]
+    if rotation_choice == 0:
+        rotation_angle = random.choice((-1, 1)) * np.random.randint(low=0, high=11, size=1)[0]
+    else:
+        rotation_angle = random.choice((-1, 1)) * np.random.randint(low=20, high=41, size=1)[0]
+    # print('Rotation')
+    # print(rotation_choice)
+    # print(rotation_angle)
+
+    scale_choice = np.random.choice(2, 1)[0]
+    if scale_choice == 0:
+        scale_percentage = np.random.uniform(0.9, 1.0, 1)[0]
+    else:
+        scale_percentage = np.random.uniform(0.5, 0.7, 1)[0]
+    # print('Scale')
+    # print(scale_choice)
+    # print(scale_percentage)
+
+    shift_choice = np.random.choice(2, 1)[0]
+    shift_x = 0
+    shift_y = 0
+    if shift_choice == 0:
+        shift_x = random.choice((-1, 1)) * np.random.randint(low=0, high=4, size=1)[0]
+        shift_y = random.choice((-1, 1)) * np.random.randint(low=0, high=4, size=1)[0]
+    else:
+        shift_x = random.choice((-1, 1)) * np.random.randint(low=6, high=11, size=1)[0]
+        shift_y = random.choice((-1, 1)) * np.random.randint(low=6, high=11, size=1)[0]
+    # print('Shift')
+    # print(shift_choice)
+    # print(shift_x)
+    # print(shift_y)
+
+    img, bbox = gen_image(img, rotation_angle, (shift_x, shift_y), scale_percentage)
+    # cv2.rectangle(img, (bbox[2], bbox[0]), (bbox[3], bbox[1]), (255, 0, 0), 1)
     # cv2.imshow('image', img)
     # cv2.waitKey(0)
     # cv2.destroyAllWindows()
+    # print([rotation_choice, scale_choice, shift_choice])
     # exit()
 
     train_images.append(img)
     train_bbox.append(bbox)
-    train_label.append(0)
-
-for i in tqdm(range(500)):
-    path = random.choice(source)
-    img = cv2.imread(path, 1)
-    dim = (90, 90)
-    img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
-    img, bbox = gen_image(img, random.choice(normal_rotation), (0, 0), random.choice(wrong_scale))
-    
-    train_images.append(img)
-    train_bbox.append(bbox)
-    train_label.append(1)
-
-for i in tqdm(range(500)):
-    path = random.choice(source)
-    img = cv2.imread(path, 1)
-    dim = (90, 90)
-    img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
-    img, bbox = gen_image(img, random.choice(wrong_rotation), (0, 0), random.choice(normal_scale))
-    
-    train_images.append(img)
-    train_bbox.append(bbox)
-    train_label.append(2)
-
-for i in tqdm(range(500)):
-    path = random.choice(source)
-    img = cv2.imread(path, 1)
-    dim = (90, 90)
-    img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
-    img, bbox = gen_image(img, random.choice(wrong_rotation), (0, 0), random.choice(wrong_scale))
-    
-    train_images.append(img)
-    train_bbox.append(bbox)
-    train_label.append(3)
+    train_label.append([rotation_choice, scale_choice, shift_choice])
 
 train_images = np.array(train_images)
 train_bbox = np.array(train_bbox)
 train_label = np.array(train_label)
+print('Test Shapes')
 np.save('./dataset/test_images.npy', train_images)
 np.save('./dataset/test_bbox.npy', train_bbox)
 np.save('./dataset/test_label.npy', train_label)
